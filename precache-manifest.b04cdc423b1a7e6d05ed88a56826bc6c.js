2a84d685ff22c3ec53456b693b099b850e23bd72
psamur <38886500+myshok@users.noreply.github.com>
2019-08-07 23:22:36 +0300

    q

---
+++ precache-manifest.b04cdc423b1a7e6d05ed88a56826bc6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapps-forest/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "a7dcc756150a2bb0ff76",
+    "url": "/vkapps-forest/static/js/runtime~main.a7dcc756.js"
+  },
+  {
+    "revision": "5762aa0ee0c11a5bcc2f",
+    "url": "/vkapps-forest/static/js/main.5762aa0e.chunk.js"
+  },
+  {
+    "revision": "c200b001b9703dff17b9",
+    "url": "/vkapps-forest/static/js/2.c200b001.chunk.js"
+  },
+  {
+    "revision": "5762aa0ee0c11a5bcc2f",
+    "url": "/vkapps-forest/static/css/main.7e9b268c.chunk.css"
+  },
+  {
+    "revision": "c200b001b9703dff17b9",
+    "url": "/vkapps-forest/static/css/2.34a9acfc.chunk.css"
+  },
+  {
+    "revision": "4374da3b58b2329699dd0b283e60c3b1",
+    "url": "/vkapps-forest/index.html"
+  }
+];
